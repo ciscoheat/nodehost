3de12c068f2064ed9a9ec01d2209d81f4470db14
Andreas <ciscoheat@gmail.com>
2017-04-10 15:14:41 +0200

    Fixed vagrant provision file.

--- provision.sh
+++ provision.sh
@@ -8,23 +8,38 @@ echo "=== Starting provision script..."
 
 cd /vagrant
 
-echo "=== Adding 'cd /vagrant' to .profile"
-cat >> /home/ubuntu/.profile <<EOL
-
-cd /vagrant
-EOL
-
 echo "=== Updating apt..."
 apt-get update >/dev/null 2>&1
 
 # Used in many dependencies:
 apt-get install python-software-properties curl git -y
 
-echo "=== Renaming host..."
-sed -i "s/`cat /etc/hostname`/nodehost/g" /etc/hosts
-echo nodehost > /etc/hostname
-systemctl restart systemd-logind.service
+echo "=== Installing Node.js 6.x..."
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash - && sudo apt-get install -y nodejs
+
+sudo -u ubuntu -i mkdir /home/ubuntu/.npm-global
+sudo -u ubuntu -i npm config set prefix '~/.npm-global'
+
+sudo -u ubuntu -i cat >> /home/ubuntu/.profile <<EOL
+PATH=~/.npm-global/bin:\$PATH
+cd /vagrant
+EOL
+
+# Install Haxe
+add-apt-repository ppa:haxe/releases -y
+apt-get update
+apt-get install haxe -y
+sudo -u ubuntu -i mkdir /home/ubuntu/haxelib 
+sudo -u ubuntu -i haxelib setup /home/ubuntu/haxelib
+
+# First time build
+sudo -u ubuntu -i npm install
+sudo -u ubuntu -i haxelib install nodehost.hxml --always --quiet
+sudo -u ubuntu -i npm run build
+sudo -u ubuntu -i npm link
+
+# Run to see if it works
+sudo -u ubuntu -i nodehost help
 
 echo "=== Provision script finished!"
-echo "Start with 'vagrant reload && vagrant ssh'."
-echo "Change timezone: sudo dpkg-reconfigure tzdata"
+echo "Start with 'vagrant ssh'"
